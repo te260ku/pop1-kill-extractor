cd499fcda9dfcb4db2492f8b328f36ca4894fa68
te260ku <teturou@aw.wakwak.com>
2022-08-20 16:11:42 +0900

    枠線を検出する方式に変更

--- scripts/main.py
+++ scripts/main.py
@@ -4,62 +4,101 @@ import numpy as np
 import time
 import sys
 
+""" 青 """
+lower_inner = np.array([143, 93, 33])
+upper_inner = np.array([160, 136, 65])
+""" 白 """
+lower_border = np.array([0,0,210])
+upper_border = np.array([169,21,233])
+
+detection_count = 0
+detection_continuation_time = 0
+detected = False
+
+
 def nothing(x):
     pass
 
-def proc(img, *current_sec):
+def proc(img, current_sec=0):
     global detection_count
     global detection_continuation_time
     global detected
+
+    font = cv2.FONT_HERSHEY_PLAIN
+    result = img.copy()
 
     # HSV変換
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
 
     # 値の範囲を指定してマスク生成
-    img_mask_raw = cv2.inRange(img_hsv, lower, upper)
+    img_mask_raw = cv2.inRange(img_hsv, lower_inner, upper_inner)
+    
 
     #中央値フィルタ
-    img_mask = cv2.medianBlur(img_mask_raw, 15)
+    img_mask = cv2.medianBlur(img_mask_raw, 5)
+    
 
-    edge = cv2.Canny(img_mask, 100, 200)
-    height, width = edge.shape
-    ksize = int(max(height, width) * 0.01)
-    ksize = ksize // 2 * 2 + 1
-    #edge = cv2.dilate(edge, np.ones((ksize, ksize), dtype='uint8'))
-    #edge = cv2.erode(edge, np.ones((ksize, ksize), dtype='uint8'))
-    edge = cv2.morphologyEx(edge, cv2.MORPH_CLOSE, np.ones((ksize, ksize), dtype='uint8'))
+    # edge = cv2.Canny(img_mask, 100, 200)
+    # height, width = edge.shape
+    # ksize = int(max(height, width) * 0.01)
+    # ksize = ksize // 2 * 2 + 1
+    # #edge = cv2.dilate(edge, np.ones((ksize, ksize), dtype='uint8'))
+    # #edge = cv2.erode(edge, np.ones((ksize, ksize), dtype='uint8'))
+    # edge = cv2.morphologyEx(edge, cv2.MORPH_CLOSE, np.ones((ksize, ksize), dtype='uint8'))
+    # contours, _ = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    contours, _ = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(
+    img_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    result = img.copy()
-    # cv2.drawContours(result, contours, -1, (255, 0, 0), 3, cv2.LINE_AA)
+    white_area_count = 0
 
-    font = cv2.FONT_HERSHEY_PLAIN
+    text = 'unknown'
 
     for cnt in contours:
-
         area = cv2.contourArea(cnt)
 
-        if (area > 20000):
-            # 検出した場合
-            arclen = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, arclen * 0.01, True)
-            
-            n_gon = len(approx)
-            if n_gon == 4:
-                text = 'rectangle'
-                cv2.drawContours(result, [approx], -1, (255, 0, 0), 3)
-                position = np.asarray(approx).reshape((-1, 2)).max(axis=0).astype('int32')
-                px, py = position
-                cv2.putText(result, text, (px + 10, py + 10), font, 2.0, (255, 255, 255), 2, cv2.LINE_AA)
+        arclen = cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, arclen * 0.01, True)
+        n_gon = len(approx)
 
-                cv2.putText(result, 'Detected', (40, 200), font, 4.0, (255, 255, 255), 2, cv2.LINE_AA)
-                if (detected == False):
-                    detected = True
-                    detection_count += 1
-                    # print(current_sec)
-            else:
-                continue
+        if (n_gon == 4):
+
+            cv2.drawContours(result, [approx], -1, (255, 0, 0), 3)
+            position = np.asarray(approx).reshape((-1, 2)).max(axis=0).astype('int32')
+            px, py = position
+            cv2.putText(result, text, (px + 10, py + 10), font, 2.0, (255, 255, 255), 2, cv2.LINE_AA)
+
+            
+            
+            if (area > 30000):
+                # 検出した場合
+                print('border area: {}'.format(area))
+                
+
+                mask = np.zeros_like(img)
+                cv2.drawContours(mask, [cnt], -1, (255, 255, 255), thickness=-1)
+                masked = img & mask
+                # HSV変換
+                img_hsv_2 = cv2.cvtColor(masked, cv2.COLOR_BGR2HSV_FULL)
+                # 値の範囲を指定してマスク生成
+                img_mask_raw_2 = cv2.inRange(img_hsv_2, lower_inner, upper_inner)
+                white_area = cv2.countNonZero(img_mask_raw_2)
+                # print('inner area: {}'.format(white_area))
+
+                white_area_count += 1
+                # cv2.imshow('image', masked)
+
+                if (white_area > 10):
+                    
+                    cv2.putText(result, 'Detected', (40, 200), font, 4.0, (255, 255, 255), 2, cv2.LINE_AA)
+
+                    if (detected == False):
+                        detected = True
+                        detection_count += 1
+                        # print(current_sec)
+            
+
+    # print(white_area_count)
 
     if (detected):
         detection_continuation_time += 1
@@ -68,14 +107,14 @@ def proc(img, *current_sec):
             detected = False
     
     cv2.putText(result, 'KILL: ' + str(detection_count), (40, 100), font, 4.0, (255, 255, 255), 2, cv2.LINE_AA)
+
+    # if (white_area_count == 0):
+    #     cv2.imshow('image', result)
+
     cv2.imshow('image', result)
 
 
-lower = np.array([143, 93, 33])
-upper = np.array([160, 136, 65])
-detection_count = 0
-detection_continuation_time = 0
-detected = False
+
 
 def proc_img(img):
     proc(img)
@@ -97,7 +136,7 @@ def proc_video(cap):
 
 def main():
     img = cv2.imread('../images/test_1.png')
-    cap = cv2.VideoCapture('../videos/test_full.mp4')
+    cap = cv2.VideoCapture('../videos/test.mp4')
 
     # ウィンドウの調整
     image_hight, image_width, image_color = img.shape
@@ -112,12 +151,12 @@ def main():
     '''************************************************************
     ** 画像
     ************************************************************'''
-    proc_img(img)
+    # proc_img(img)
 
     '''************************************************************
     ** 動画
     ************************************************************'''
-    # proc_video(cap)
+    proc_video(cap)
 
     cv2.destroyAllWindows()
 
