f7f38a35351d2a2bd20ac08cf4d5e0d83b604c14
te260ku <teturou@aw.wakwak.com>
2022-08-25 16:02:50 +0900

    UIパーツをコンポーネント化

--- scripts/gui.py
+++ scripts/gui.py
@@ -6,39 +6,53 @@ import numpy as np
 gui_font = 'Meiryo UI'
 
 def draw():
+
+    processing = False
     '''************************************************************
     ** レイアウトの定義
     ************************************************************'''
     sg.theme('LightBlue')
 
+    file_select_button_comp = sg.FileBrowse('ファイルを選択', key='inputFilePath')
+
+    start_button_comp = sg.Button('開始', key='startButton')
+    stop_button_comp = sg.Button('停止', key='stopButton')
+    exit_button_comp = sg.Button('アプリ終了', key='exitButton')
+    preview_checkbox_comp = sg.Checkbox('プレビュー表示', False, key='previewCheckbox', font=(gui_font, 13))
+    processing_status_text_comp = sg.Text('Waiting...', key='processingStatusText', font=(gui_font, 13))
+
+    preview_image_comp = sg.Image(filename='../images/initial_image.png', key='previewImage')
+
+    kill_count_text_comp = sg.Text('', key='killCountText', font=(gui_font, 13))
+    fps_text_comp = sg.Text('', key='fpsText', font=(gui_font, 13))
+    kill_time_log_comp = sg.Output(size=(80,12))
+    copy_button_comp = sg.Button('コピー', key='copyButton')
+    save_button_comp = sg.Button('停止', key='saveButton')
+
     layout_1 = [  
-        [sg.Text('ファイル'), sg.Input(), sg.FileBrowse('ファイルを選択', key='inputFilePath')], 
-        [sg.Button('開始', key='startButton'), sg.Button('停止', key='stopButton'), sg.Button('アプリ終了', key='exitButton')], 
-        [sg.Checkbox('プレビュー表示', False, key='previewCheckbox', font=(gui_font, 13))],  
-        # [sg.Text('', key='analysisStatus')], 
-        [sg.Text('', key='killCountText', font=(gui_font, 13))], 
-        [sg.Text('', key='fpsText', font=(gui_font, 13))], 
+        [sg.Text('ファイル'), sg.Input(), file_select_button_comp], 
+        [start_button_comp, stop_button_comp, exit_button_comp], 
+        [preview_checkbox_comp],  
+        [processing_status_text_comp], 
     ]
 
     layout_2 = [
-        [sg.Image(filename='', key='previewImage')]
+        [preview_image_comp], 
     ]
 
     layout_3 = [
-        [sg.Output(size=(80,20))], 
-        [sg.Button('コピー', key='copyButton'), sg.Button('停止', key='saveButton')]
+        [kill_count_text_comp, fps_text_comp], 
+        [kill_time_log_comp],  
+        [copy_button_comp, save_button_comp]
     ]
 
 
-
-
     layout=[
-        [sg.Frame('Group 1', layout_1, size=(480, 200))], 
+        [sg.Frame('Setting', layout_1, size=(1920*0.2+200+10, 150))], 
         [
-            sg.Frame('Group 2', layout_2, size=(1920*0.2, 1080*0.2+50)), 
-            sg.Frame('Group 3', layout_3, size=(230, 1080*0.2+50)), 
+            sg.Frame('Preview', layout_2, size=(1920*0.2, 1080*0.2+100)), 
+            sg.Frame('Log', layout_3, size=(200, 1080*0.2+100)), 
         ], 
-        
     ]
                 
     # ウィンドウの生成
@@ -57,26 +71,32 @@ def draw():
         if event == 'exitButton' or event == sg.WIN_CLOSED:
             break
 
-        ret, frame = cap.read()
-
-        if not ret:
-            continue
-
-        current_sec = cap.get(cv2.CAP_PROP_POS_MSEC)
-
-        frame = main.proc(frame, current_sec=current_sec)
-        frame = cv2.resize(frame, dsize=None, fx=0.2, fy=0.2)
-        
-        if values['previewCheckbox']:
-            bytes = cv2.imencode('.png', frame)[1].tobytes()
-            window['previewImage'].update(data=bytes)
-        else:
-            tmp = np.zeros_like(frame)
-            bytes = cv2.imencode('.png', tmp)[1].tobytes()
-            window['previewImage'].update(data=bytes)
-
-        if event == 'copyButton':
+        if event == 'startButton':
+            processing = True
+            window['processingStatusText'].update('Processing...')
+        elif event == 'stopButton':
+            processing = False
+            window['processingStatusText'].update('Waiting...')
+        elif event == 'copyButton':
             main.copy_kill_time()
+
+        if (processing == True):
+            ret, frame = cap.read()
+            if not ret:
+                continue
+
+            current_sec = cap.get(cv2.CAP_PROP_POS_MSEC)
+
+            frame = main.proc(frame, current_sec=current_sec)
+            frame = cv2.resize(frame, dsize=None, fx=0.2, fy=0.2)
+            
+            if values['previewCheckbox']:
+                bytes = cv2.imencode('.png', frame)[1].tobytes()
+                window['previewImage'].update(data=bytes)
+            else:
+                tmp = np.zeros_like(frame)
+                bytes = cv2.imencode('.png', tmp)[1].tobytes()
+                window['previewImage'].update(data=bytes)
 
         kill_count_text = 'キル数: {}'.format(main.detection_count)
         window['killCountText'].update(kill_count_text)
