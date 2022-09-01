6a1ca60b10f53eff0900fd7f4b1f8a8e4e2b63a4
te260ku <teturou@aw.wakwak.com>
2022-09-01 19:08:12 +0900

    輪郭内の面積が条件を満たしているもののみ頂点数を計算するように変更

--- scripts/main.py
+++ scripts/main.py
@@ -80,84 +80,85 @@ def proc(img, current_sec=0):
     for cnt in contours:
         # 輪郭内の領域の面積を計算
         inner_area = cv2.contourArea(cnt)
-        # 輪郭を近似して頂点数を計算
-        arclen = cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, arclen * 0.02, True)
-        n_gon = len(approx)
 
-        
         # 輪郭が四角形かつ指定した値よりも大きい面積かどうかを判定
-        if (n_gon == 4 and 30000 < inner_area < 40000):
-        # if (30000 < inner_area):
-            """ キルログ内部の領域の二値化画像に膨張処理をかけて枠線を含むマスク画像を生成する """
-            img_inner_mask = np.zeros_like(img)   # 内部の二値化画像
-            cv2.drawContours(img_inner_mask, [cnt], -1, (255, 255, 255), thickness=-1)
-            kernel = np.ones((12,12),np.uint8)
-            img_dilation = cv2.dilate(img_inner_mask, kernel, iterations=1)   # 内部の二値化膨張画像
-            img_inner_color = cv2.bitwise_and(img, img_inner_mask)  # 内部のカラー画像
-            img_inner_color_dilation = cv2.bitwise_and(img, img_dilation)   # 内部のカラー膨張画像
-            img_border = cv2.bitwise_xor(img_inner_color, img_inner_color_dilation)  # 枠のカラー
-            # HSV変換
-            img_hsv_border = cv2.cvtColor(img_border, cv2.COLOR_BGR2HSV_FULL)
-            # マスク画像を生成
-            img_mask_border = cv2.inRange(img_hsv_border, lower_border, upper_border)
-            # マスク画像中のキルログの枠線の面積を計算
-            white_area = cv2.countNonZero(img_mask_border)
+        if (30000 < inner_area < 40000):
+            # 輪郭を近似して頂点数を計算
+            arclen = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, arclen * 0.02, True)
+            n_gon = len(approx)
 
-            # return (img_inner_color_dilation, estimated_kill_time)
+            if (n_gon == 4):
+            # if (30000 < inner_area):
+                """ キルログ内部の領域の二値化画像に膨張処理をかけて枠線を含むマスク画像を生成する """
+                img_inner_mask = np.zeros_like(img)   # 内部の二値化画像
+                cv2.drawContours(img_inner_mask, [cnt], -1, (255, 255, 255), thickness=-1)
+                kernel = np.ones((12,12),np.uint8)
+                img_dilation = cv2.dilate(img_inner_mask, kernel, iterations=1)   # 内部の二値化膨張画像
+                img_inner_color = cv2.bitwise_and(img, img_inner_mask)  # 内部のカラー画像
+                img_inner_color_dilation = cv2.bitwise_and(img, img_dilation)   # 内部のカラー膨張画像
+                img_border = cv2.bitwise_xor(img_inner_color, img_inner_color_dilation)  # 枠のカラー
+                # HSV変換
+                img_hsv_border = cv2.cvtColor(img_border, cv2.COLOR_BGR2HSV_FULL)
+                # マスク画像を生成
+                img_mask_border = cv2.inRange(img_hsv_border, lower_border, upper_border)
+                # マスク画像中のキルログの枠線の面積を計算
+                white_area = cv2.countNonZero(img_mask_border)
 
-
-            # 射影変換
-            # p_original = np.float32(approx)
-            # min_x = 10
-            # min_y = 10
-            # w = 800
-            # h = 100
-            # p_trans = np.float32([[min_x,min_y+h], [min_x+w,min_y+h], [min_x+w,min_y], [min_x,min_y]])
-            # rows, cols, _ = result.shape
-            # M = cv2.getPerspectiveTransform(p_original, p_trans)
-            # img_trans = cv2.warpPerspective(img_inner_color_dilation, M, (cols, rows))
-            # # HSV変換
-            # img_hsv_trans = cv2.cvtColor(img_trans, cv2.COLOR_BGR2HSV_FULL)
-            # # マスク画像を生成
-            # img_mask_trans = cv2.inRange(img_hsv_trans, lower_border, upper_border)
-            # # マスク画像中のキルログの枠線の面積を計算
-            # white_area_trans = cv2.countNonZero(img_mask_trans)
+                # return (img_inner_color_dilation, estimated_kill_time)
 
 
-            # cv2.drawContours(result, [approx], -1, (255, 0, 0), 8)
-            # print('inner area: {}'.format(inner_area))
-            # print('border area: {}'.format(white_area))
+                # 射影変換
+                # p_original = np.float32(approx)
+                # min_x = 10
+                # min_y = 10
+                # w = 800
+                # h = 100
+                # p_trans = np.float32([[min_x,min_y+h], [min_x+w,min_y+h], [min_x+w,min_y], [min_x,min_y]])
+                # rows, cols, _ = result.shape
+                # M = cv2.getPerspectiveTransform(p_original, p_trans)
+                # img_trans = cv2.warpPerspective(img_inner_color_dilation, M, (cols, rows))
+                # # HSV変換
+                # img_hsv_trans = cv2.cvtColor(img_trans, cv2.COLOR_BGR2HSV_FULL)
+                # # マスク画像を生成
+                # img_mask_trans = cv2.inRange(img_hsv_trans, lower_border, upper_border)
+                # # マスク画像中のキルログの枠線の面積を計算
+                # white_area_trans = cv2.countNonZero(img_mask_trans)
 
-            # cv2.imwrite('../tmp/' + str(frame_count) + '.png', img_inner_color_dilation)
 
-
-            # 検出された白い枠線の面積が指定した範囲内だった場合、キルログとして判定
-            # if (1200 < white_area_trans):
-            # if (400 < white_area < 500):
-            if (3500 < white_area):
-                # 背景によってはインフォーメーションログを誤検出する場合がある
-
-                # 輪郭とテキストを描画する
-                cv2.drawContours(result, [approx], -1, (255, 0, 0), 8)
-                # position = np.asarray(approx).reshape((-1, 2)).max(axis=0).astype('int32')
-                # px, py = position
-                # cv2.putText(result, 'rec', (px + 10, py + 10), font, 1.0, (255, 255, 255), 2, cv2.LINE_AA)
-
-                cv2.putText(result, 'Detected', (10, 130), font, 1.0, (0, 255, 0), 2)
-
+                # cv2.drawContours(result, [approx], -1, (255, 0, 0), 8)
                 # print('inner area: {}'.format(inner_area))
                 # print('border area: {}'.format(white_area))
 
-                if (detected == False):
-                    detected = True
-                    detection_count += 1
-                    estimated_kill_time = '{{:.{:d}f}}'.format(2).format(current_sec/1000)
-                    kill_time.append(estimated_kill_time)
-                    print('Estimated Kill Time: {}'.format(estimated_kill_time))
-                    cv2.putText(result, str(inner_area), (10, 30), font, 1.0, (0, 255, 0), thickness=2)
-                    cv2.putText(result, str(white_area), (10, 80), font, 1.0, (0, 255, 0), 2)
-                    cv2.imwrite('../tmp/' + str(frame_count) + '.png', result)
+                # cv2.imwrite('../tmp/' + str(frame_count) + '.png', img_inner_color_dilation)
+
+
+                # 検出された白い枠線の面積が指定した範囲内だった場合、キルログとして判定
+                # if (1200 < white_area_trans):
+                # if (400 < white_area < 500):
+                if (3500 < white_area):
+                    # 背景によってはインフォーメーションログを誤検出する場合がある
+
+                    # 輪郭とテキストを描画する
+                    cv2.drawContours(result, [approx], -1, (255, 0, 0), 8)
+                    # position = np.asarray(approx).reshape((-1, 2)).max(axis=0).astype('int32')
+                    # px, py = position
+                    # cv2.putText(result, 'rec', (px + 10, py + 10), font, 1.0, (255, 255, 255), 2, cv2.LINE_AA)
+
+                    cv2.putText(result, 'Detected', (10, 130), font, 1.0, (0, 255, 0), 2)
+
+                    # print('inner area: {}'.format(inner_area))
+                    # print('border area: {}'.format(white_area))
+
+                    if (detected == False):
+                        detected = True
+                        detection_count += 1
+                        estimated_kill_time = '{{:.{:d}f}}'.format(2).format(current_sec/1000)
+                        kill_time.append(estimated_kill_time)
+                        print('Estimated Kill Time: {}'.format(estimated_kill_time))
+                        cv2.putText(result, str(inner_area), (10, 30), font, 1.0, (0, 255, 0), thickness=2)
+                        cv2.putText(result, str(white_area), (10, 80), font, 1.0, (0, 255, 0), 2)
+                        cv2.imwrite('../tmp/' + str(frame_count) + '.png', result)
 
     """ 検出の重複を防ぐための処理 """
     if (detected):
