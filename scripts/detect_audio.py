d149f6c8f1bfbe3c99dc651f118119062f3a2685
te260ku <teturou@aw.wakwak.com>
2023-09-16 00:59:50 +0900

    特定の周波数帯域の振幅の統計量をもとに検出する機能を作成

--- scripts/detect_audio.py
+++ scripts/detect_audio.py
@@ -1,13 +1,77 @@
 import librosa
 import numpy as np
 from moviepy.editor import VideoFileClip, concatenate_videoclips
+import os
+from os.path import dirname
+import scipy.signal
+from scipy import fftpack
 
-
-# input_video_file = '../videos/sample_1_trim.mp4'
-output_video_file = '../videos/output.mp4'
-kill_audio_file = './audio/kill_audio_1.wav'
+input_video_file = '../videos/sample_2_trim.wav'
+kill_audio_file = '../audio/kill_audio_1.wav'
+# kill_audio_file = './audio/kill_audio_2.wav'
 trimmed_sorted_peak_times = []
 concat_detect_create_video_checkbox_changed = True
+processing_status_text = ""
+
+
+import librosa
+import numpy as np
+
+def fft_ave(data, Fs):
+    fft = np.fft.fft(data)                      # FFT（実部と虚部）
+    fft_amp = np.abs(fft / (Fs / 2))            # 振幅成分を計算
+    return fft_amp
+
+
+def calc_fft_mean_in_window(target_sound_file, game_audio_file, min_freq, max_freq, segment):
+    # 効果音の読み込み
+    target_sound, sr_target = librosa.load(target_sound_file, sr=None)
+    
+    # ゲーム音声の読み込み
+    game_audio, sr_game = librosa.load(game_audio_file, sr=None)
+
+    game_audio = game_audio[segment[0]:segment[1]]
+    # game_audio = game_audio[565136:829735]
+
+    
+    # 効果音の長さを取得
+    target_sound_length = len(target_sound)
+    
+    # 窓のサイズを効果音と同じに設定
+    window_size = target_sound_length
+    
+    # 窓をスライドさせるステップサイズ
+    step_size = 512
+    
+    # 平均値を保存するリスト
+    mean_values = []
+    std_values = []
+    
+    # ゲーム音声を窓で切り出して平均値を計算
+    for i in range(0, len(game_audio) - window_size + 1, step_size):
+        windowed_audio = game_audio[i:i + window_size]
+        
+        # ウィンドウ内の平均を計算
+        fft_result = fft_ave(windowed_audio, sr_game)
+        frequencies = np.fft.fftfreq(len(fft_result), 1.0 / sr_game)
+        min_bin = np.argmin(np.abs(frequencies - min_freq))
+        max_bin = np.argmin(np.abs(frequencies - max_freq))
+        
+        # 指定した周波数帯域内の振幅を取得
+        extracted = fft_result[min_bin:max_bin]
+        
+        # 指定した周波数帯域内の平均値を計算
+        mean_value = np.mean(extracted)
+        mean_values.append(mean_value)
+
+        std_value = np.std(extracted)
+        std_values.append(std_value)
+    
+    return mean_values, std_values
+
+
+
+
 
 
 def cut_video(input_file, output_file, segments):
@@ -33,12 +97,15 @@ def extract_kill_audio_time(input_video_file):
     # ゲーム音声の読み込み
     game_audio, sr_game = librosa.load(input_video_file, sr=None)
 
+    print(len(game_audio))
 
-    print("効果音を探索しています")
+
+    print("効果音を検索しています")
     # クロスコレレーションを計算して、効果音の再生位置を見つける
+    # cross_correlations = np.correlate(game_audio, target_sound, mode='full')
     cross_correlations = np.correlate(game_audio, target_sound, mode='full')
     # クロスコレレーションのピークを検出
-    peak_indices = np.argsort(cross_correlations)[-5:]
+    peak_indices = np.argsort(cross_correlations)[-100:]
     peak_times = peak_indices / sr_game
     sorted_peak_times = np.sort(peak_times)
     
@@ -52,6 +119,18 @@ def extract_kill_audio_time(input_video_file):
             j += 1
         i = j
 
+    
+
+
+
+
+
+
+
+
+
+
+
 
     if len(peak_indices) > 0:
         print("効果音が以下の時間に再生されました：")
@@ -59,13 +138,33 @@ def extract_kill_audio_time(input_video_file):
         for peak_time in trimmed_sorted_peak_times:
             minutes = int(peak_time // 60)
             seconds = int(peak_time % 60)
-            cross_correlation = cross_correlations[peak_indices[i]]
-            print(f"{minutes}分 {seconds}秒 cross_correlation: {cross_correlation}")
+            # cross_correlation = cross_correlations[peak_indices[i]]
+            
+
+
+
+            
+            min_freq = 32
+            max_freq = 64
+
+            # 平均値を計算
+            segment = (int((peak_time-2)*sr_game), int((peak_time+2)*sr_game))
+            mean_values, std_values = calc_fft_mean_in_window(kill_audio_file, input_video_file, min_freq, max_freq, segment)
+
+            threshold_mean = 0.02
+            threshold_std = 0.02
+
+            for i, (mean_value, std_value) in enumerate(zip(mean_values, std_values)):
+                if mean_value >= threshold_mean and std_value <= threshold_std:
+                    print(f"time:{minutes}:{seconds} / frame:{segment[0]} - {segment[1]} / mean:{mean_value} / std:{std_value}")
+                    break
+
+
+
             i += 1
     else:
         print("効果音が見つかりませんでした")
 
-    create_video(input_video_file)
 
 
 def create_video(input_video_file):
@@ -73,4 +172,8 @@ def create_video(input_video_file):
     segments = []
     for peak_time in trimmed_sorted_peak_times:
         segments.append((int(peak_time-3), int(peak_time+3)))
+    output_video_file = os.path.join(dirname(input_video_file), 'output.mp4')
     cut_video(input_video_file, output_video_file, segments)
+
+
+extract_kill_audio_time(input_video_file)
